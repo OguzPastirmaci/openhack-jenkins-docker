a21a372a8b2946955e7c1a1a3b8d3fe10a52e2c0
Oguz Pastirmaci <oguzp@Oguzs-MacBook-Pro.local>
2018-07-18 10:22:03 -0700

    Update script location

--- deploy.sh
+++ deploy.sh
@@ -13,17 +13,17 @@ then
     # Create a new virtual machine, this creates SSH keys if not present.
     az vm create --resource-group $resourceGroup --name $virtualMachine --admin-username $adminUser --image UbuntuLTS --generate-ssh-keys
 
-    # Open port 22 to allow web traffic to host.
+    # Open port 22
     az vm open-port --port 80 --resource-group $resourceGroup --name $virtualMachine  --priority 101
 
-    # Open port 80 to allow web traffic to host.
+    # Open port 80
     az vm open-port --port 22 --resource-group $resourceGroup --name $virtualMachine --priority 102
 
-    # Open port 8080 to allow web traffic to host.
+    # Open port 8080
     az vm open-port --port 8080 --resource-group $resourceGroup --name $virtualMachine --priority 103
 
     # Use CustomScript extension to install.
-    az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $virtualMachine --resource-group $resourceGroup --settings '{"fileUris": ["https://raw.githubusercontent.com/OguzPastirmaci/openhack-jenkins-vm/master/config1.sh"],"commandToExecute": "./config1.sh"}'
+    az vm extension set --publisher Microsoft.Azure.Extensions --version 2.0 --name CustomScript --vm-name $virtualMachine --resource-group $resourceGroup --settings '{"fileUris": ["https://raw.githubusercontent.com/OguzPastirmaci/openhack-jenkins-docker/master/config.sh"],"commandToExecute": "./config.sh"}'
 
     # Get public IP
     ip=$(az vm list-ip-addresses --resource-group $resourceGroup --name $virtualMachine --query [0].virtualMachine.network.publicIpAddresses[0].ipAddress -o tsv)
