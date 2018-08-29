fe6bf0aba49d91d6ef3e3e1c2e3fd7799f839b63
Oguz Pastirmaci <oguzp@Oguzs-MBP.guest.corp.microsoft.com>
2018-08-29 10:06:31 -0400

    Change approach

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 FROM jenkins/jenkins:lts
+
+ENV JAVA_OPTS="-Djenkins.install.runSetupWizard=false"
+
+COPY default-user.groovy /usr/share/jenkins/ref/init.groovy.d/default-user.groovy
+
 RUN /usr/local/bin/install-plugins.sh azure-acs blueocean credentials-binding git ghprb kubernetes pipeline-github-lib workflow-aggregator workflow-job azure-credentials docker-plugin
-
-ENV JENKINS_USER jenkins
-
-ENV JAVA_OPTS -Djenkins.install.runSetupWizard=false
-
-COPY default-user.groovy /usr/share/jenkins/ref/init.groovy.d/
--- default-user.groovy
+++ default-user.groovy
@@ -1,13 +1,17 @@
 #!groovy
-
-def env = System.getenv()
+ 
+import jenkins.model.*
+import hudson.security.*
+import jenkins.security.s2m.AdminWhitelistRule
+ 
 def instance = Jenkins.getInstance()
-
+ 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+hudsonRealm.createAccount("admin", env.JENKINS_PASS)
 instance.setSecurityRealm(hudsonRealm)
-
+ 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
 instance.setAuthorizationStrategy(strategy)
 instance.save()
+ 
+Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class).setMasterKillSwitch(false)
