e4d9101399529e75414167a483bf9b466ac264fd
Oguz Pastirmaci <oguzp@microsoft.com>
2018-08-29 11:29:29 -0400

    Update default-user.groovy

--- default-user.groovy
+++ default-user.groovy
@@ -1,13 +1,14 @@
-#!groovy
+import jenkins.model.*
+import hudson.security.*
 
 def env = System.getenv()
-def instance = Jenkins.getInstance()
 
-def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount(env.JENKINS_USER, env.JENKINS_PASS)
-instance.setSecurityRealm(hudsonRealm)
+def jenkins = Jenkins.getInstance()
+jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+jenkins.setAuthorizationStrategy(new GlobalMatrixAuthorizationStrategy())
 
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(false)
-instance.setAuthorizationStrategy(strategy)
-instance.save()
+def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
+user.save()
+
+jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+jenkins.save()
